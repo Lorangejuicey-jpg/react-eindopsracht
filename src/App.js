d05a19241a4b950acf5ccf104d9f93fc6c5321d1
LorangeJuicey <60689696+LorangeJuicey@users.noreply.github.com>
2022-04-04 11:44:00 +0200

    version-2

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 import "./App.css";
 
 const App = () => {
+  fetch("https://pokeapi.co/api/v2/berry/1/")
+    .then((res) => res.json())
+    .then((result) => console.log(result))
+    .catch(console.log("error"));
+
   return <div className="App"></div>;
 };
 
