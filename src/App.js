daf63ebb797db061d9fba2c7627be3e9a88e88fa
LorangeJuicey <60689696+LorangeJuicey@users.noreply.github.com>
2022-04-22 14:36:52 +0200

    edit routing

--- src/App.js
+++ src/App.js
@@ -1,69 +1,15 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
-import React, { useEffect, useState } from "react";
-// import Link from 'link-react';
+import Berry from "./Components/berries/berry";
+// import React, { useEffect, useState } from "react";
 const App = () => {
-  const [results, setResults] = useState({});
-  const [resultsSprites, setResultsSprites] = useState({});
-  const [value, setValue] = useState({});
-
-  const fetchData = (value = 1) => {
-    fetch(`https://pokeapi.co/api/v2/berry/` + value)
-      .then((response) => {
-        return response.json();
-      })
-      .then((result) => {
-        console.log(result);
-        setResults(result);
-      });
-  };
-
-  const fetchSprite = () => {
-    fetch(results?.item?.url)
-      .then((response) => {
-        return response.json();
-      })
-      .then((resultSprite) => {
-        console.log(resultSprite);
-        setResultsSprites(resultSprite);
-      });
-  };
-  useEffect(() => {
-    fetchData();
-  }, []);
-  useEffect(() => {
-    fetchSprite();
-  }, [results]);
-
   return (
     <div>
-      <div className="input-container">
-        <input
-          placeholder="Enter an berry id"
-          type="text"
-          onChange={(e) => {
-            setValue(e.target.value);
-          }}
-        ></input>
-        <button type="submit" onClick={() => fetchData(value)}>
-          Search
-        </button>
-      </div>
-      <div className="berry-info">
-        <h2>Name: {results?.item?.name}</h2>
-        <span>Firmness: {results?.firmness?.name}</span>
-        <span>Pokedollars: {resultsSprites?.cost}</span>
-        <span>natural gift type: {results?.natural_gift_type?.name}</span>
-        <span>
-          <strong>Effect:</strong>
-        </span>
-        {/* <span>{resultsSprites?.effect_entries[0]?.effect}</span> */}
-        <div className="sprite-container">
-          <img
-            className="berry-sprite"
-            src={resultsSprites?.sprites?.default}
-          ></img>
-        </div>
-      </div>
+      {/* <link to={`berryInfo`}>Go to berries</link> */}
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/berryInfo" element={<Berry />} />
+      </Routes>
     </div>
   );
 };
---
+++ src/Components/berries/berry.jsx
@@ -0,0 +1,76 @@
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+
+const Berry = () => {
+  const [results, setResults] = useState({});
+  const [resultsSprites, setResultsSprites] = useState({});
+  const [value, setValue] = useState({});
+
+  const fetchData = (value = 1) => {
+    fetch(`https://pokeapi.co/api/v2/berry/` + value)
+      .then((response) => {
+        return response.json();
+      })
+      .then((result) => {
+        console.log(result);
+        setResults(result);
+      });
+  };
+
+  const fetchSprite = () => {
+    fetch(results?.item?.url)
+      .then((response) => {
+        return response.json();
+      })
+      .then((resultSprite) => {
+        console.log(resultSprite);
+        setResultsSprites(resultSprite);
+      });
+  };
+  useEffect(() => {
+    fetchData();
+  }, []);
+  useEffect(() => {
+    fetchSprite();
+  }, [results]);
+  try {
+    return (
+      <div>
+        <div className="input-container">
+          <Link to="/">Homepagina</Link>
+          <Link to="/berryInfo">Berry info</Link>
+          <input
+            placeholder="Enter an berry id"
+            type="text"
+            onChange={(e) => {
+              setValue(e.target.value);
+            }}
+          ></input>
+          <button type="submit" onClick={() => fetchData(value)}>
+            Search
+          </button>
+        </div>
+        <div className="berry-info">
+          <h2>Name: {results?.item?.name}</h2>
+          <span>Firmness: {results?.firmness?.name}</span>
+          <span>Pokedollars: {resultsSprites?.cost}</span>
+          <span>natural gift type: {results?.natural_gift_type?.name}</span>
+          <span>
+            <strong>Effect:</strong>
+          </span>
+          <span>{resultsSprites?.effect_entries[0]?.effect}</span>
+          <div className="sprite-container">
+            <img
+              className="berry-sprite"
+              src={resultsSprites?.sprites?.default}
+            ></img>
+          </div>
+        </div>
+      </div>
+    );
+  } catch {
+    console.log("JoE mAmA");
+  }
+};
+
+export default Berry;
